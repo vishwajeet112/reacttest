f0270e603c7efb93cf09131c905e5b65a4a09871
Vishwajeet <81477266+vishwajeet112@users.noreply.github.com>
2023-08-07 18:20:17 +0530

    hello

---
+++ precache-manifest.ef6b93583459905d8c2584e18a022301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "129890da5ceaf887af16",
+    "url": "/Text-editor/static/css/main.f916775e.chunk.css"
+  },
+  {
+    "revision": "129890da5ceaf887af16",
+    "url": "/Text-editor/static/js/main.129890da.chunk.js"
+  },
+  {
+    "revision": "66d3df16377a19ed444f",
+    "url": "/Text-editor/static/js/1.66d3df16.chunk.js"
+  },
+  {
+    "revision": "8b4d57c9d0ac7fdfb829",
+    "url": "/Text-editor/static/js/2.8b4d57c9.chunk.js"
+  },
+  {
+    "revision": "d97a5259ed9ae6477402",
+    "url": "/Text-editor/static/js/runtime~main.d97a5259.js"
+  },
+  {
+    "revision": "165a2ff5302e453d6d39e82a3d88eab4",
+    "url": "/Text-editor/index.html"
+  }
+];
